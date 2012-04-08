d8bd3417b439d134cad25d7c8b8c314dacc3e631
Paul Mederos <paul@primeenterprise.net>
2012-04-08 02:04:09 -0400

    Front-end fleshed out. Need to figure out URLs, routes, and templates.

--- pkcomm/pkcomm/settings.py
+++ pkcomm/pkcomm/settings.py
@@ -59,11 +59,11 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = '/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
@@ -118,6 +118,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+		'pkcomm'
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
